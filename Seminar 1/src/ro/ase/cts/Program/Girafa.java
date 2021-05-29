0ca576d4f2d5d2826836393555143d09760e2ff8
Stan-SebastianDaniel <71817431+Stan-SebastianDaniel@users.noreply.github.com>
2021-05-29 08:31:17 +0300

    Add files via upload

---
+++ Seminar 1/src/ro/ase/cts/Program/Girafa.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.Program;
+
+public class Girafa extends Animal{
+    public Girafa(String name) {
+        super(name);
+    }
+}
---
+++ Seminar 1/src/ro/ase/cts/Program/Main.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.Program;
+
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        Girafa g1 = new Girafa("Girafa1");
+        Girafa g2 = new Girafa("Girafa2");
+
+        zoo.Add(g1);
+        zoo.Add(g2);
+
+        zoo.feedAllAnimals();
+    }
+}
